0711a2cbc9fe11ca0eea8aa232ac7b3bff782c87
Rae Crusoe <113061355+mantarae-22@users.noreply.github.com>
2025-05-28 12:14:43 -0700

    Add files via upload

---
+++ w3info.js
@@ -0,0 +1,17 @@
+// single line comment
+/*
+multi-line comment
+*/
+var firstName = "Rae"; // sometimes you give the variable a starting value
+let lastName; // sometimes you don't know the variable's value until later, but you need to create it
+// this is valid JavaScript syntax to create a variable, but please do not create your variables like this
+// when you create a variable like this, it is not clear where the variable first comes into existence
+pizza = "yummy";
+
+// JavaScript is dynamically (loosely) typed, so JavaScript guesses what the datatype is for your variables
+// this can cause issues with adding numbers if JavaScript thinks your numbers are strings
+
+// add the h2 text using JavaScript
+document.getElementById("h2subtitle").textContent = "JavaScript is cool.";
+// add the paragraph text
+document.getElementById("pMoreInfo").textContent = "JavaScript is but is dynamically typed, so -1 points for that.";
